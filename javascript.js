f7568e7dbfafed91d7975685e1a924aaede0e8f0
Harathi-Ramoju <104606529+Harathi-Ramoju@users.noreply.github.com>
2024-03-03 20:55:15 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// script.js
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // You can add your code to handle form submission (e.g., send an email) here
+    alert("Your message has been sent!");
+  });
+  
